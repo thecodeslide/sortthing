6b53dfaf6496809f4d69588080dd248c94bb2138
FC <45506833+thecodeslide@users.noreply.github.com>
2019-10-21 21:57:01 +0900

    chagne to column and add finishing

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function main() {
         for(const el of document.getElementsByClassName("number")) {
                 console.log(el.textContent);
                 let size = parseInt(el.textContent) * 6 ;
-                el.setAttribute("style", "width:" + size + "px");
+                el.setAttribute("style", "height:" + size + "px");
         }
 }
 
@@ -15,35 +15,47 @@ function toggle(pre, curr) {
         curr.classList.toggle("active");
 }
 
+function toggleFinish(curr) {
+        curr.classList.toggle("finish");
+
+}
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function sort() {
-        let changed = true;
-        let elems = document.getElementsByClassName("number")
-        // let elems = Array.from(document.getElementsByClassName("number"))
-        let container = document.getElementById("container")
-        while (changed) {
-                changed = false;
-                let i = 1;
-                        while( i < elems.length) {
+    let changed = true;
+    let current;
+    let elems = document.getElementsByClassName("number")
+    // let elems = Array.from(document.getElementsByClassName("number"))
+    let container = document.getElementById("container")
+    let j = 0;
+    while (changed) {
+        changed = false;
+        let i = 1;
 
-                                let current = elems[i];
-                                let previous = elems[i-1];
-                                toggle(previous, current);
+        while( i < elems.length - j) {
 
-                                if (parseInt(previous.textContent) > parseInt(current.textContent)) {
-                                        changed = true;
-                                        // elems.splice(i - 1, 1);
-                                        container.insertBefore(current, previous);
-                                        // previous.remove();
-                                }
-                                i++;
-                                await sleep(500)
-                                toggle(previous, current);
-                        // container
+            current = elems[i];
+            let previous = elems[i-1];
+            toggle(previous, current);
 
-                        }
-        }
+            if (parseInt(previous.textContent) > parseInt(current.textContent)) {
+                    changed = true;
+                    // elems.splice(i - 1, 1);
+                    container.insertBefore(current, previous);
+                    // previous.remove();
+            }
+            i++;
+            await sleep(500)
+            toggle(previous, current);
+    // container
+
+            }
+            toggleFinish(elems[elems.length - j - 1]);
+            await sleep(300);
+            toggleFinish(elems[elems.length - j - 1]);
+        j++;
+    }
 }
